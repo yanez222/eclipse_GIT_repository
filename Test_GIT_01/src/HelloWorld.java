b3a90ec872d45a3f37a97a0b8244c25c453878e9
Paolo Mazza <paolo.mazza@alcatel-lucent.com>
2015-08-27 14:48:45 +0200

    commit !!!

---
+++ Test_GIT_01/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("HelloWorld");
+
+    }
+
+    public int print()
+    {
+        return 4;
+    }
+
+}
